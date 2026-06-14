989b08635833cb9fdc424625b698e59a1065f23a
bullywyrm-byte <bullywyrm@gmail.com>
2026-06-14 13:14:21 +0200

    Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
@@ -10,4 +10,4 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000
   }
-})
+});
